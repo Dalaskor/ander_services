8f147ef73e6a2f2f37663f61a0fc235727f44d32
Dalaskor <dalas.prog@gmail.com>
2022-12-21 14:13:41 +0400

    Complete header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-// const sum = require('./module/sum.js');
+import $ from "jquery";
+
+$(document).ready(function () {
+  // Бургер меню
+  $(".header__burger").click(() => {
+    $(".mobile-menu").toggleClass("open-menu");
+    $(".header__burger").toggleClass("opened");
+    $("html").toggleClass("scroll-lock");
+  });
+  $(".mobile-menu__link").click(() => {
+    $(".mobile-menu").removeClass("open-menu");
+    $(".header__burger").removeClass("opened");
+    $("html").removeClass("scroll-lock");
+  });
+  $(".mobile-menu__btn").click(() => {
+    $(".mobile-menu").removeClass("open-menu");
+    $(".header__burger").removeClass("opened");
+    $("html").removeClass("scroll-lock");
+  });
+});
